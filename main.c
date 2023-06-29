69dad61f753aaf121360b381e306a42866a73dea
AusSS7 <riolesteban05@gmail.com>
2023-06-29 04:30:55 -0700

    C - printf

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - entry point
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+
+
+}
---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef PRINTF_H
+#def PRINTF_H
+
+int _printf(const char *format, ...);
+
+#endif
